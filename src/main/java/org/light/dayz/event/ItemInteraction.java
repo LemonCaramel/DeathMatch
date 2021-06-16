e30305fbdca022158fd83081255e9c16fcbc2392
LemonCaramel <admin@caramel.moe>
2021-06-16 13:48:16 +0900

    상호작용 문제 해결

--- src/main/java/org/light/dayz/event/ItemInteraction.java
+++ src/main/java/org/light/dayz/event/ItemInteraction.java
@@ -45,58 +45,57 @@ public class ItemInteraction implements Listener {
         Player p = event.getPlayer();
         Location loc = p.getLocation().clone();
         ItemStack stack = p.getInventory().getItemInMainHand();
-        if (stack != null && stack.getType() != Material.AIR && stack.getItemMeta().getDisplayName() != null) {
+        if (stack.getType() == Material.AIR) return;
+        String name = stack.getItemMeta().getDisplayName();
+        if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && (name.contains("붕대") || name.contains("구급상자") || name.contains("에너지 드링크") || name.contains("치료제"))) {
             event.setCancelled(true);
-            String name = stack.getItemMeta().getDisplayName();
-            if ((event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) && (name.contains("붕대") || name.contains("구급상자") || name.contains("에너지 드링크") || name.contains("치료제"))) {
-                if (p.hasPotionEffect(PotionEffectType.FAST_DIGGING) && p.hasPotionEffect(PotionEffectType.SLOW))
-                    p.sendMessage("§c[ §f! §c] §b이미 아이템을 사용중입니다.");
+            if (p.hasPotionEffect(PotionEffectType.FAST_DIGGING) && p.hasPotionEffect(PotionEffectType.SLOW))
+                p.sendMessage("§c[ §f! §c] §b이미 아이템을 사용중입니다.");
+            else {
+                if (stack.getAmount() > 1) {
+                    stack.setAmount(stack.getAmount() - 1);
+                    p.getInventory().setItemInMainHand(stack);
+                }
+                else
+                    p.getInventory().setItemInMainHand(null);
+                if (name.contains("구급상자")) {
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 100, 1, true, false), false);
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100, 100, true, false), false);
+                    p.sendMessage("§c[ §f! §c] §f구급 상자를 사용 중입니다.. (5초 | 3칸 이상 멀어질시 취소)");
+                    Bukkit.getScheduler().runTaskLater(Plugin, () -> {
+                        if (GameController.contains(p.getUniqueId()) && loc.distance(p.getLocation()) <= 3.0) {
+                            p.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 1, 5, true, true), true);
+                        }
+                        else if (GameController.contains(p.getUniqueId()) && loc.distance(p.getLocation()) > 3.0) {
+                            ItemStack clone = stack.clone();
+                            clone.setAmount(1);
+                            p.sendMessage("§c거리가 너무 멀어져 사용이 취소되었습니다.");
+                            p.getInventory().addItem(clone);
+                        }
+                    }, 100L);
+                }
                 else {
-                    if (stack.getAmount() > 1) {
-                        stack.setAmount(stack.getAmount() - 1);
-                        p.getInventory().setItemInMainHand(stack);
-                    }
-                    else
-                        p.getInventory().setItemInMainHand(null);
-                    if (name.contains("구급상자")) {
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 100, 1, true, false), false);
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100, 100, true, false), false);
-                        p.sendMessage("§c[ §f! §c] §f구급 상자를 사용 중입니다.. (5초 | 3칸 이상 멀어질시 취소)");
-                        Bukkit.getScheduler().runTaskLater(Plugin, () -> {
-                            if (GameController.contains(p.getUniqueId()) && loc.distance(p.getLocation()) <= 3.0) {
-                                p.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 1, 5, true, true), true);
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 60, 1, true, false), false);
+                    p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60, 100, true, false), false);
+                    p.sendMessage("§c[ §f! §c] §f아이템을 사용 중입니다.. (5초 | 3칸 이상 멀어질시 취소)");
+                    Bukkit.getScheduler().runTaskLater(Plugin, () -> {
+                        if (GameController.contains(p.getUniqueId()) && loc.distance(p.getLocation()) <= 3.0) {
+                            if (name.contains("붕대"))
+                                p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 25, 7, true, true), true);
+                            else if (name.contains("에너지 드링크"))
+                                p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 900, 2, true, true), true);
+                            else {
+                                p.removePotionEffect(PotionEffectType.SLOW);
+                                p.removePotionEffect(PotionEffectType.LUCK);
                             }
-                            else if (GameController.contains(p.getUniqueId()) && loc.distance(p.getLocation()) > 3.0) {
-                                ItemStack clone = stack.clone();
-                                clone.setAmount(1);
-                                p.sendMessage("§c거리가 너무 멀어져 사용이 취소되었습니다.");
-                                p.getInventory().addItem(clone);
-                            }
-                        }, 100L);
-                    }
-                    else {
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 60, 1, true, false), false);
-                        p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 60, 100, true, false), false);
-                        p.sendMessage("§c[ §f! §c] §f아이템을 사용 중입니다.. (5초 | 3칸 이상 멀어질시 취소)");
-                        Bukkit.getScheduler().runTaskLater(Plugin, () -> {
-                            if (GameController.contains(p.getUniqueId()) && loc.distance(p.getLocation()) <= 3.0) {
-                                if (name.contains("붕대"))
-                                    p.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 25, 7, true, true), true);
-                                else if (name.contains("에너지 드링크"))
-                                    p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 900, 2, true, true), true);
-                                else {
-                                    p.removePotionEffect(PotionEffectType.SLOW);
-                                    p.removePotionEffect(PotionEffectType.LUCK);
-                                }
-                            }
-                            else if (GameController.contains(p.getUniqueId()) && loc.distance(p.getLocation()) > 3.0) {
-                                ItemStack clone = stack.clone();
-                                clone.setAmount(1);
-                                p.sendMessage("§c거리가 너무 멀어져 사용이 취소되었습니다.");
-                                p.getInventory().addItem(clone);
-                            }
-                        }, 60L);
-                    }
+                        }
+                        else if (GameController.contains(p.getUniqueId()) && loc.distance(p.getLocation()) > 3.0) {
+                            ItemStack clone = stack.clone();
+                            clone.setAmount(1);
+                            p.sendMessage("§c거리가 너무 멀어져 사용이 취소되었습니다.");
+                            p.getInventory().addItem(clone);
+                        }
+                    }, 60L);
                 }
             }
         }
